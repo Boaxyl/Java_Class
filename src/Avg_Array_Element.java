f3e2d6cae241efe56e177f6dc861f5f85e6935ee
Boaxyl <79419405+Boaxyl@users.noreply.github.com>
2022-06-26 22:02:19 +0000

    Adjustment made today 26/06/2022

--- src/Avg_Array_Element.java
+++ src/Avg_Array_Element.java
@@ -1,13 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Avg_Array_Element {
     public static void main(String[] args) {
         //A Java program to calculate the average value of array elements.
 
-        int[] Marks = {11, 17, 23, 35, 44, 53, 65, 72};
+        System.out.println("Enter the length of the array :");
+        Scanner input = new Scanner(System.in);
+
+        int num = input.nextInt();
+
+        int [] Marks = new int[num];
+
+        System.out.println("Enter the " + num + " numbers now");
+        for (int i = 0; i < Marks.length; i++) {
+            Marks[i] = input.nextInt();
+        }
+        System.out.println();System.out.println("These are the numbers you entered.");
+        System.out.println(Arrays.toString(Marks));
+
+        input.close();
         //calculate the sum of all array elements
-        int sum = 0;
+        float sum = 0;
         for (int mark : Marks) sum = sum + mark;
         //calculate average value
-        double average = sum / Marks.length;
+        float average = sum / Marks.length;
         System.out.println("The sum of the numbers in the array = " + sum);
         System.out.println("The length of the array = " +  Marks.length);
         System.out.println("The average value of the array elements = " + average);
